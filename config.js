1988e98c3da1064441cd4bd07185e0f46036c882
aayushmaannarsaria <aayushmaannarsaria@gmail.com>
2018-02-18 05:22:32 +0530

    config changed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'mongoUrl' : process.env.MONGOLAB_URI || 'mongodb://localhost:27017/conFusion',
     'facebook': {
         clientID: '894321897346426',
         clientSecret: 'c99302168d3b9198d2e235b6135f82ac',
